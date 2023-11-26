bb1e1cab496f4b5ce0e9edbe748bda0722d9aca2
Rawle Becerril <rawlesxm@gmail.com>
2023-11-26 18:50:38 -0500

    error message customization

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -21,8 +21,13 @@ const NewIssuePage = () => {
   return (
     <form className="max-w-xl space-y-3" 
     onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/issues', data)
-        router.push('/issues')
+        try {
+            await axios.post('/api/issues', data)
+            router.push('/issues')
+        } catch (error) {
+            console.log(error)
+        }
+
     })}>
         <TextField.Root>
             <TextField.Input placeholder="Title" {...register('title')}/>
