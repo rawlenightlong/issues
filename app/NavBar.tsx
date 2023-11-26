e42795cbd83f5e3aa9b2bb2fccb69a12bf5c092e
Rawle Becerril <rawlesxm@gmail.com>
2023-11-26 16:36:53 -0500

    style active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
 import { FaBug } from "react-icons/fa6";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 const NavBar = () => {
+
+    const currentPath = usePathname()
 
     const links = [
         {label: "Dashboard", href: "/"},
@@ -17,7 +22,11 @@ const NavBar = () => {
             <Link 
             key={link.href}
             href={link.href}
-            className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            className={classNames({
+                'text-zinc-900': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+            })}
             >
                 {link.label}
             </Link>)}
