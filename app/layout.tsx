33a3debd91cb76cb34e4c62dd2834744cb3e9e21
Rawle Becerril <rawlesxm@gmail.com>
2023-11-26 17:54:04 -0500

    new issue button

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,8 +22,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+      <Theme>
         <NavBar/>
         <main>{children}</main>
+        </Theme>
       </body>
     </html>
   )
